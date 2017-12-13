2dcdd2fe57dabb787e95deaae4433345c8d8d387
liuhuaming2017 <33945540+liuhuaming2017@users.noreply.github.com>
2017-12-13 15:35:37 +0800

    Update ConsumerDelegate.java

--- src/main/java/com/service/consumer/controller/ConsumerDelegate.java
+++ src/main/java/com/service/consumer/controller/ConsumerDelegate.java
@@ -1,14 +1,23 @@
 package com.service.consumer.controller;
 
 import org.springframework.stereotype.Component;
-
+import io.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ConsumerDelegate {
-
+    
+    private static RestTemplate restTemplate = RestTemplateBuilder.create();
+    
     public String helloworld(String name){
 
         // Do Some Magic Here!
-        return name;
+         String helloworld = restTemplate.getForObject("cse://provider1/helloworld?name="+name, null, String.class);
+        return helloworld;
+    }
+    public String getuname(String name,String test){
+        // Do Some Magic Here!         
+        String helloworld = restTemplate.getForObject("cse://provider1/getuname?name="+name + "&test=" + test, null, String.class);
+        return helloworld;
     }
 }
